12f0b904e0d3c32f14944d4c2437bcbe1e60e2b8
Glen Lohja <76854498+GlenLohja@users.noreply.github.com>
2024-12-02 08:52:44 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -46,12 +46,12 @@ export default function Home() {
 
       const res = await fetch(`https://api.themoviedb.org/3/${media_type}/${id}`, {
         method: 'GET',
-        headers: { 'Authorization': `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MDY5ZmVhOTU5YWJmMmNjNDY1ZTAzMDIzY2ZkMGRmMCIsIm5iZiI6MTczMjc4NDMxNS45Mjc4NzI3LCJzdWIiOiI2NzQwOTQ5MTVjYWMwNDFjZmFlMjgxODIiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.vJNBXM5_i9wgt1cunX51nV9ti8wdqWCPL7ZPZWkHir8` },
+        headers: { 'Authorization': `Bearer` },
       });
       const details = await res.json();
       const video  = await fetch(`https://api.themoviedb.org/3/${media_type}/${id}/videos`, {
         method: 'GET',
-        headers: { 'Authorization': `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MDY5ZmVhOTU5YWJmMmNjNDY1ZTAzMDIzY2ZkMGRmMCIsIm5iZiI6MTczMjc4NDMxNS45Mjc4NzI3LCJzdWIiOiI2NzQwOTQ5MTVjYWMwNDFjZmFlMjgxODIiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.vJNBXM5_i9wgt1cunX51nV9ti8wdqWCPL7ZPZWkHir8` },
+        headers: { 'Authorization': `Bearer ` },
       });
 
       const video_detail = await video.json();
