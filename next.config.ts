e18caf586330d364983d5561834e7c014a1b5480
Glen Lohja <glenilohja@yahoo.com>
2024-12-04 18:25:20 +0100

    add images.remotePatterns in next config instead of images.domains

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,23 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["ui-avatars.com", 'image.tmdb.org', 'demos.creative-tim.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
+        pathname: '/t/p/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'ui-avatars.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'demos.creative-tim.com',
+        pathname: '/**',
+      },
+    ],
   },
 };
 
