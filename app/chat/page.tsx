4deb3e7bd1f4e19be336f4d327c44dda16cec05a
Glen Lohja <glenilohja@yahoo.com>
2024-12-05 13:26:14 +0100

    test new changes

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Conversation from '../components/Conversation';
 import Messages from '../components/Messages';
 import { useAuth } from '../context/AuthContext';
@@ -28,217 +28,228 @@ interface Room {
 }
 
 const Chat = () => {
-    const [search, setSearch] = useState("");
-    const [searchResults, setSearchResults] = useState([]);
-    const { user, isAuthenticated, loading } : any = useAuth()
-    const [rooms, setRooms] = useState<Room[]>([]);
-    const [selectedRoomId, setSelectedRoomId] = useState("");
-    const [selectedRoomName, setSelectedRoomName] = useState("");
-    const [messages, setMessages] = useState<any[]>([]);
-    const [token, setToken] = useState('');
-    const [userId, setUserId] = useState('');
-    const router = useRouter();
+  const [search, setSearch] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const { user, isAuthenticated, loading }: any = useAuth();
+  const [rooms, setRooms] = useState<Room[]>([]);
+  const [selectedRoomId, setSelectedRoomId] = useState("");
+  const [selectedRoomName, setSelectedRoomName] = useState("");
+  const [messages, setMessages] = useState<any[]>([]);
+  const [token, setToken] = useState('');
+  const [userId, setUserId] = useState('');
+  const router = useRouter();
 
-    const socketClient: Socket = io("https://movie-project-bk-630243095989.us-central1.run.app", {
+  // Create a ref to hold the socket client instance
+  const socketClientRef = useRef<Socket | null>(null);
+
+  const handleSearchChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      const searchValue = e.target.value;
+      setSearch(searchValue);
+
+      if (searchValue.trim() === "") {
+        setSearchResults([]);
+        return;
+      }
+
+      try {
+        const res = await fetch(`https://movie-project-bk-630243095989.us-central1.run.app/api/search-users/?search=${searchValue}`, {
+          method: 'GET',
+          headers: { 'Authorization': `Bearer ${token}` },
+        });
+
+        if (res.ok) {
+          const details = await res.json();
+          if (details.message) {
+            setSearchResults([]);
+          } else {
+            setSearchResults(details);
+          }
+        } else {
+          setSearchResults([]);
+        }
+      } catch (error) {
+        console.error('Error searching users:', error);
+        setSearchResults([]);
+      }
+    }
+  };
+
+  const fetchRooms = async () => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      try {
+        const response = await apiClient.get("/rooms", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        if (!response.data) {
+          throw new Error("Failed to fetch rooms");
+        }
+        setRooms(response.data);
+        if (response.data.length > 0) {
+          setSelectedRoomId(response.data[response.data.length - 1].id);
+          setSelectedRoomName(response.data[response.data.length - 1].name);
+        }
+      } catch (error) {
+        console.error("Error fetching rooms:", error);
+      }
+    }
+  };
+
+  const fetchMessages = async (roomId: string) => {
+    try {
+      const response = await apiClient.get(`/messages/${roomId}`);
+      setMessages(response.data);
+    } catch (error) {
+      console.error("Failed to fetch messages:", error);
+      toast.error("Failed to fetch messages");
+    }
+  };
+
+  const handleUserClick = async (user: any) => {
+    try {
+      const token = localStorage.getItem('token');
+      if (!token) return;
+
+      const res = await apiClient.post(
+        '/rooms',
+        { userId: user.id, email: user.email },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      if (res.status === 201) {
+        fetchRooms();
+      }
+    } catch (error) {
+      console.error('Error creating room:', error);
+    }
+  };
+
+  const handleJoinRoom = (roomId: string, name: string) => {
+    setSelectedRoomName(name);
+    setSelectedRoomId(roomId);
+    setUserId(user?.id);
+    fetchMessages(roomId);
+
+    if (socketClientRef.current) {
+      socketClientRef.current.emit("join", { roomId });
+      socketClientRef.current.on("message", (message) => {
+        setMessages((prevMessages) => [...prevMessages, message]);
+      });
+    }
+  };
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    setToken(token || '');
+  }, [user]);
+
+  useEffect(() => {
+    if (!loading && !isAuthenticated) {
+      router.push('/login');
+    }
+
+    if (token !== "") {
+      fetchRooms();
+    }
+  }, [isAuthenticated, loading, router, token]);
+
+  useEffect(() => {
+    if (token && !socketClientRef.current) {
+      socketClientRef.current = io("https://movie-project-bk-630243095989.us-central1.run.app", {
         withCredentials: true,
         extraHeaders: {
           "Authorization": `Bearer ${token}`,
         },
-        // transports: ['websocket']
-    });
-
-    
-    const handleSearchChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
-        const token = localStorage.getItem('token');
-        if (token) {
-            const searchValue = e.target.value;
-            setSearch(searchValue);
-
-            if (searchValue.trim() === "") {
-                setSearchResults([]);
-                return;
-            }
-
-            try {
-                const res = await fetch(`https://movie-project-bk-630243095989.us-central1.run.app/api/search-users/?search=${searchValue}`, {
-                    method: 'GET',
-                    headers: { 'Authorization': `Bearer ${token}` },
-                    });
-        
-                    if (res.ok) {
-                        const details = await res.json();
-
-                        if (details.message) {
-
-                            setSearchResults([]);
-                        } else {
-                            setSearchResults(details);
-
-                        }
-                    } else {
-                        setSearchResults([]);
-                    }
-            } catch (error) {
-                console.error('Error searching users:', error);
-                setSearchResults([]);
-            }
-        }
-    };
-
-    const fetchRooms = async () => {
-        const token = localStorage.getItem('token');
-        if (token) {
-            try {
-                const response = await apiClient.get("/rooms", {
-                    headers: {
-                        Authorization: `Bearer ${token}`,
-                    },
-                });
-                if (!response.data) {
-                    throw new Error("Failed to fetch rooms");
-                }
-                console.log(response.data);
-                setRooms(response.data);
-                if (response.data.length > 0) {
-
-                    setSelectedRoomId(response.data[response.data.length - 1].id);
-                    setSelectedRoomName(response.data[response.data.length - 1].name);
-                }
-            } catch (error) {
-                console.error("Error fetching rooms:", error);
-            }
-        }
-    };
-
-    const fetchMessages = async (roomId: string) => {
-        try {
-          const response = await apiClient.get(`/messages/${roomId}`);
-          console.log(response);  
-          setMessages(response.data);
-        } catch (error) {
-          console.error("Failed to fetch messages:", error);
-          toast.error("Failed to fetch messages");
-        }
-    };
-
-    const handleUserClick = async (user: any) => {
-        try {
-          const token = localStorage.getItem('token');
-          if (!token) return;
-    
-          const res = await apiClient.post(
-            '/rooms',
-            { userId: user.id, email: user.email },
-            {
-              headers: {
-                Authorization: `Bearer ${token}`,
-              },
-            }
-          );
-    
-          if (res.status === 201) {
-            fetchRooms();
-          }
-        } catch (error) {
-          console.error('Error creating room:', error);
-        }
-        
-    };
-
-    const handleJoinRoom = (roomId: string, name: string) => {
-
-        setSelectedRoomName(name);
-        setSelectedRoomId(roomId);
-        setUserId(user?.id);
-        fetchMessages(roomId);
-        socketClient.emit("join", { roomId });
-        socketClient.on("message", (message) => {
-            setMessages((prevMessages) => [...prevMessages, message]);
-        });
-
-        return () => {
-            socketClient.off("message");
-        };
-    };
-    
-    useEffect(() => {
-        const token = localStorage.getItem('token');
-        setToken(token || '');
-    }, [user]);
-
-    useEffect(() => {
-      if (!loading && !isAuthenticated) {
-        router.push('/login');
-      } 
-
-      if (token !== "") {
-
-        fetchRooms();
+      });
+  
+      socketClientRef.current.on('connect', () => {
+        console.log('Socket connected successfully');
+      });
+  
+      socketClientRef.current.on('connect_error', (err) => {
+        console.error('Socket connection error:', err);
+      });
+  
+      socketClientRef.current.on('disconnect', () => {
+        console.log('Socket disconnected');
+      });
+    }
+  
+    return () => {
+      if (socketClientRef.current) {
+        socketClientRef.current.disconnect();
       }
-    }, [isAuthenticated, loading, router]);
+    };
+  }, [token]);
 
-
-      
-    return (
-      <div className="">
-        <div className="flex">
-          <div className="w-80 px-4 hidden md:block border-r-[1px] border-selfMessage">
-            <div className="overflow-y-auto">
-              <div className="text-xl text-gray-600 dark:text-gray-200 p-3">Chat</div>
-              <div className="search-chat flex p-3">
-                <input
-                  className="input text-gray-700 dark:text-gray-200 text-sm p-3 focus:outline-none bg-selfMessage dark:bg-selfMessage w-full rounded-l-md"
-                  type="text"
-                  value={search}
-                  onChange={handleSearchChange}
-                  placeholder="Search for a user"
-                />
-                <div className="bg-gray-200 dark:bg-selfMessage flex justify-center items-center pr-3 text-gray-400 rounded-r-md">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-6 w-5"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                    />
-                  </svg>
-                </div>
+  return (
+    <div className="">
+      <div className="flex">
+        <div className="w-80 px-4 hidden md:block border-r-[1px] border-selfMessage">
+          <div className="overflow-y-auto">
+            <div className="text-xl text-gray-600 dark:text-gray-200 p-3">Chat</div>
+            <div className="search-chat flex p-3">
+              <input
+                className="input text-gray-700 dark:text-gray-200 text-sm p-3 focus:outline-none bg-selfMessage dark:bg-selfMessage w-full rounded-l-md"
+                type="text"
+                value={search}
+                onChange={handleSearchChange}
+                placeholder="Search for a user"
+              />
+              <div className="bg-gray-200 dark:bg-selfMessage flex justify-center items-center pr-3 text-gray-400 rounded-r-md">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                  />
+                </svg>
               </div>
-                {searchResults.length !== 0 && (
-                    <div className="p-3">
-                        <div className="text-lg text-gray-600 dark:text-gray-200">Search Results</div>
-                        <ul>
-                            {searchResults.map((user: any) => (
-                                <li
-                                    key={user.id}
-                                    className="p-2 hover:bg-gray-200 cursor-pointer dark:hover:bg-gray-800 rounded"
-                                    onClick={() => handleUserClick(user)}
-                                    >
-                                    {user.name} ({user.email})
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
-                )}
-              <div className="text-lg text-gray-600 dark:text-gray-200 p-3">Recent</div>
-              <Conversation rooms={rooms.slice().reverse()} handleJoinRoom={handleJoinRoom}/>
             </div>
-          </div>
-            {userId && selectedRoomName && messages && (
-            <div className="flex-grow p-2 rounded-md">
-                    <Messages id={userId} room_name={selectedRoomName} messages={messages} />
-                    <MessageInput roomId={selectedRoomId} />
-            </div>
+            {searchResults.length !== 0 && (
+              <div className="p-3">
+                <div className="text-lg text-gray-600 dark:text-gray-200">Search Results</div>
+                <ul>
+                  {searchResults.map((user: any) => (
+                    <li
+                      key={user.id}
+                      className="p-2 hover:bg-gray-200 cursor-pointer dark:hover:bg-gray-800 rounded"
+                      onClick={() => handleUserClick(user)}
+                    >
+                      {user.name} ({user.email})
+                    </li>
+                  ))}
+                </ul>
+              </div>
             )}
+            <div className="text-lg text-gray-600 dark:text-gray-200 p-3">Recent</div>
+            <Conversation rooms={rooms.slice().reverse()} handleJoinRoom={handleJoinRoom} />
+          </div>
         </div>
+        {userId && selectedRoomName && messages && (
+          <div className="flex-grow p-2 rounded-md">
+            <Messages id={userId} room_name={selectedRoomName} messages={messages} />
+            <MessageInput roomId={selectedRoomId} socketClient={socketClientRef.current}/>
+          </div>
+        )}
       </div>
-    );
+    </div>
+  );
 };
 
 export default Chat;
--- app/components/MessageInput.tsx
+++ app/components/MessageInput.tsx
@@ -1,40 +1,35 @@
-import { io, Socket } from "socket.io-client";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { IoSend } from "react-icons/io5";
 import { useMovie } from '../context/MovieContext';
 import { Library } from '../types/Library';
-import { useAuth } from "../context/AuthContext";
+import { Socket } from 'socket.io-client'; // Import Socket type
 
-
-const MessageInput: React.FC<{ roomId: string }> = ({ roomId }) => {
+const MessageInput: React.FC<{ roomId: string, socketClient: Socket | null }> = ({ roomId, socketClient }) => {
     const [text, setText] = useState("");
-    const [token, setToken] = useState('');
-    const { user, isAuthenticated, loading } : any = useAuth()
     const [selectedLibraryId, setSelectedLibraryId] = useState<string | null>(null);
     const [selectedLibraryName, setSelectedLibraryName] = useState<string | null>(null);
-    //, ]
-    const {
-        userLibraries
-    }: any = useMovie();
-    
-    const socketClient: Socket = io("https://movie-project-bk-630243095989.us-central1.run.app", {
-        withCredentials: true,
-        extraHeaders: {
-          "Authorization": `Bearer ${token}`,
-        },
-        // transports: [ 'polling']
-    });
+    const { userLibraries }: any = useMovie();
+
     const handleSendMessage = () => {
-        if (text) {
+        console.log("socketClient:", socketClient); // Log socketClient to check its state
+        if (text && socketClient) {
             socketClient.emit("message", {
                 roomId,
                 senderId: socketClient.id,
                 text,
             });
-            setText("");
-        } else if (selectedLibraryId) {
+            setText(""); // Reset input after sending message
+        } else if (selectedLibraryId && selectedLibraryName && socketClient) {
             const link = `http://localhost:3000/library/${selectedLibraryId}?library_name=${selectedLibraryName}`;
             setText(link);
+            socketClient.emit("message", {
+                roomId,
+                senderId: socketClient.id,
+                text: link,
+            });
+            // Reset library selection after sending message
+            setSelectedLibraryId(null);
+            setSelectedLibraryName(null);
         }
     };
 
@@ -46,18 +41,11 @@ const MessageInput: React.FC<{ roomId: string }> = ({ roomId }) => {
     };
 
     const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-        
         const id = e.target.value;
-        const name = e.target.options[e.target.selectedIndex].text
+        const name = e.target.options[e.target.selectedIndex].text;
         setSelectedLibraryName(name);
         setSelectedLibraryId(id);
-    }
-
-    useEffect(() => {
-        const token = localStorage.getItem('token');
-        setToken(token || '');
-    }, []);
-
+    };
 
     return (
         <div className="py-4 px-6 flex rounded-2xl">
@@ -73,15 +61,15 @@ const MessageInput: React.FC<{ roomId: string }> = ({ roomId }) => {
                 id="countries"
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-2xl focus:ring-blue-500 focus:border-blue-500 block ml-2 p-2.5 dark:bg-white-700 dark:border-gray-600 dark:placeholder-black-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 onChange={handleChange}
-                >
-                <option defaultValue="">Choose a library</option>
-
+                value={selectedLibraryId || ''}
+            >
+                <option value="">Choose a library</option>
                 {userLibraries.map((library: Library) => (
-                    <option key={library.id} value={library.id} >
+                    <option key={library.id} value={library.id}>
                         {library.name}
                     </option>
                 ))}
-             </select>
+            </select>
             <button
                 onClick={handleSendMessage}
                 className="bg-netflixRed hover:bg-blue-500 text-white font-bold py-2.5 px-4 rounded- ml-2 rounded-2xl"
